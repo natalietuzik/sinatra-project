c14a46e2d9c04ccf256e2be2157a27f953fdefa9
Natalie T <natalietuzik@gmail.com>
2025-03-11 02:14:20 +0000

    accidentally did it all in one go oopsies

--- app.rb
+++ app.rb
@@ -1,9 +1,60 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
+require "json"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  redirect("/food_form")
+end
+
+get("/food_form") do
+  erb(:food_form)
+end
+
+get("/get_foods") do
+  begin
+    @ingredient_one = params.fetch("ingredient_one").capitalize
+    @ingredient_two = params.fetch("ingredient_two").capitalize
+    @ingredient_three = params.fetch("ingredient_three").capitalize
+  rescue KeyError => e
+    #Redirect message
+    redirect("/food_form?error=#{e.message}")
+  end
+ 
+  api_url = "https://api.spoonacular.com/recipes/findByIngredients/?apiKey=#{ENV["RECIPE_API_KEY"]}&ingredients=#{@ingredient_one},+#{@ingredient_two},+#{@ingredient_three}&number=20"
+
+  raw_data = HTTP.get(api_url)
+
+  
+  raw_data_string = raw_data.to_s
+
+
+  @parsed_data = JSON.parse(raw_data_string)
+  
+  
+  erb(:get_foods)
+end
+
+get("/:get_foods/:get_recipe") do
+  @recipe_id = params.fetch(:get_recipe)
+
+  api_url = "https://api.spoonacular.com/recipes/#{@recipe_id}/information?apiKey=#{ENV["RECIPE_API_KEY"]}"
+
+
+  raw_data = HTTP.get(api_url)
+
+  
+  raw_data_string = raw_data.to_s
+
+  
+  @parsed_recipe = JSON.parse(raw_data_string)
+
+  @title = @parsed_recipe.fetch("title")
+
+  @instructions = @parsed_recipe.fetch("analyzedInstructions")
+  @steps = @instructions.first.fetch("steps")
+
+  @img_url = @parsed_recipe.fetch("image")
+  
+  erb(:get_recipe)
 end
